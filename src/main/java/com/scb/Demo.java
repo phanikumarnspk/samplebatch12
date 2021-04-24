be84bf23e83972ff33df9fb4abc122b9e832b585
phanikumarnspk <54536843+phanikumarnspk@users.noreply.github.com>
2021-04-24 09:40:24 +0530

    Update Demo.java

--- src/main/java/com/scb/Demo.java
+++ src/main/java/com/scb/Demo.java
@@ -3,7 +3,7 @@ package com.scb;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
 public class Demo {
 
 	@GetMapping("/demo")
